cbda4a87ae1cb622394b2247ae0ad9ae520ddde7
Alex Tseng <636479@aacps.org>
2020-10-14 14:37:34 +0000

    Got rid of practice code

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,38 +2,38 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
         TerrapinCard cardBilly = new TerrapinCard(20);
-        // TerrapinCard cardBrian = new TerrapinCard(30);
+        TerrapinCard cardBrian = new TerrapinCard(30);
+
+        cardBilly.payGourmet();
+        cardBrian.payEconomical();
+
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
+
+        cardBilly.loadMoney(20);
+        cardBrian.payGourmet();
+
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
+
+        cardBilly.payEconomical();
+        cardBilly.payEconomical();
+        cardBrian.loadMoney(50);
+
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
 
         // cardBilly.payGourmet();
-        // cardBrian.payEconomical();
+        // cardBilly.payGourmet();
+        // cardBilly.payGourmet();
+        // cardBilly.payGourmet();
+        // cardBilly.payGourmet();
+        // cardBilly.payGourmet();
+        // cardBilly.loadMoney(10);
+        // cardBilly.loadMoney(-10);
+        // cardBilly.loadMoney(10);
+        // cardBilly.loadMoney(140);
 
-        // System.out.println("Billy: " + cardBilly);
-        // System.out.println("Brian: " + cardBrian);
-
-        // cardBilly.loadMoney(20);
-        // cardBrian.payGourmet();
-
-        // System.out.println("Billy: " + cardBilly);
-        // System.out.println("Brian: " + cardBrian);
-
-        // cardBilly.payEconomical();
-        // cardBilly.payEconomical();
-        // cardBrian.loadMoney(50);
-
-        // System.out.println("Billy: " + cardBilly);
-        // System.out.println("Brian: " + cardBrian);
-
-        cardBilly.payGourmet();
-        cardBilly.payGourmet();
-        cardBilly.payGourmet();
-        cardBilly.payGourmet();
-        cardBilly.payGourmet();
-        cardBilly.payGourmet();
-        cardBilly.loadMoney(10);
-        cardBilly.loadMoney(-10);
-        cardBilly.loadMoney(10);
-        cardBilly.loadMoney(140);
-
-        System.out.println(cardBilly);
+        // System.out.println(cardBilly);
     }
 }
