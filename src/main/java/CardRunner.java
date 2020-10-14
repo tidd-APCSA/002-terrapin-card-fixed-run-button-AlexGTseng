62af6d6d682a82f75eb3025d779ec7c5f93b30a3
Alex Tseng <636479@aacps.org>
2020-10-14 13:40:44 +0000

    I made the terrapiCard class and put the code in the main class

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,34 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        System.out.println("hey");
+        
+        // TerrapinCard card = new TerrapinCard(50);
+        // System.out.println(card);
+        // card.payGourmet();
+        // System.out.println(card);
+        // card.loadMoney(-10);
+        // System.out.println(card);
 
+        TerrapinCard cardBilly = new TerrapinCard(20);
+        TerrapinCard cardBrian = new TerrapinCard(30);
+
+        cardBilly.payGourmet();
+        cardBrian.payEconomical();
+
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
+
+        cardBilly.loadMoney(20);
+        cardBrian.payGourmet();
+
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
+
+        cardBilly.payEconomical();
+        cardBilly.payEconomical();
+        cardBrian.loadMoney(50);
+
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,38 @@
 public class TerrapinCard {
+  private double balance;
 
+  public TerrapinCard(double balanceAtStart) {
+    balance = balanceAtStart;
+  }
 
+  public void payEconomical() {
+    if(balance >= 2.50) {
+      balance -= 2.50;
+    } else {
+      balance -= 0;
+    }
+  }
+
+  public void payGourmet() {
+    if(balance >= 4.00) {
+      balance -= 4.00;
+    } else {
+      balance -= 0;
+    }
+  }
+
+  public void loadMoney(double amount) {
+    if(amount >= 0) {
+      balance += amount;
+    }
+
+    if(balance > 150.0) {
+      balance = 150.0;
+    }
+  }
+
+  public String toString() {
+    return "The card has " + balance + " dollars";
+  }
 }
+
