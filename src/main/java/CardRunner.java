b5b92791930d7add3de8df16432252c078245b39
Alex Tseng <636479@aacps.org>
2020-10-14 13:43:04 +0000

    I got rid of unnecessary code

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,14 +1,6 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        
-        // TerrapinCard card = new TerrapinCard(50);
-        // System.out.println(card);
-        // card.payGourmet();
-        // System.out.println(card);
-        // card.loadMoney(-10);
-        // System.out.println(card);
-
         TerrapinCard cardBilly = new TerrapinCard(20);
         TerrapinCard cardBrian = new TerrapinCard(30);
 
